379b1872caa34a3113e1408837f1823ebadfc6d4
KonneyJ <konnova.j@yandex.ru>
2025-10-25 16:04:40 +0300

    Добавлено решение Задача: Сортировка массива

---
+++ src/main/java/org/konneyj/BubbleSorted.java
@@ -0,0 +1,39 @@
+package org.konneyj;
+
+import java.util.Random;
+
+public class BubbleSorted {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[random.nextInt(5, 20)];
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(1000);
+        }
+
+        System.out.println("Исходный массив длиной " + n + " элементов:");
+        String str = "";
+        for (int num : arr) {
+            str += num + ", ";
+        }
+        System.out.println(str);
+
+        for (int i = n - 1; i >= 1 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Отсортированный массив:");
+        String str1 = "";
+        for (int num : arr) {
+            str1 += num + ", ";
+        }
+        System.out.println(str1);
+    }
+}
