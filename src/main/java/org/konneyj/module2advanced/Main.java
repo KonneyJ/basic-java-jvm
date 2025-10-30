e4524e38eb0c456714b05582850d42e57b3ba4bc
KonneyJ <konnova.j@yandex.ru>
2025-10-30 15:58:56 +0300

    Добавлено решение Задача: Контракт Объекта (equals() и hashCode())

--- src/main/java/org/konneyj/module2advanced/Main.java
+++ src/main/java/org/konneyj/module2advanced/Main.java
@@ -1,7 +1,17 @@
 package org.konneyj.module2advanced;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        checkReliableClass();
+        checkEqualAndHashCode();
+    }
+
+    public static void checkReliableClass() {
         try {
             BankAccount account = new BankAccount("", 1000);
         } catch (IllegalArgumentException e) {
@@ -22,5 +32,25 @@ public class Main {
             System.out.println("Получено исключение " + e.getClass());
             System.out.println(e.getMessage());
         }
+        System.out.println("-".repeat(50));
+    }
+
+    public static void checkEqualAndHashCode() {
+        Currency currency1 = new Currency("USD", "Dollar");
+        Currency currency2 = new Currency("USD", "US Dollar");
+        Currency currentcy3 = new Currency("EUR", "Euro");
+
+        System.out.println("Проверка объектов на равенство: " + currency1.equals(currency2));
+        System.out.println("Проверка объектов на равенство: " + currency1.equals(currentcy3));
+        System.out.println("Проверка объектов на равенство: " + currency2.equals(currentcy3));
+
+        Set<Currency> currencies = new HashSet<>();
+        currencies.add(currency1);
+        currencies.add(currency2);
+        System.out.println("Set должен содержать один элемент: " + currencies.size());
+        currencies.add(currentcy3);
+        System.out.println("Set должен содержать два элемента: " + currencies.size());
+
+        System.out.println("-".repeat(50));
     }
 }
