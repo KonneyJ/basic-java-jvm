31299aa70892f01e6e295e9034e22d9fd7b86314
KonneyJ <konnova.j@yandex.ru>
2025-11-06 16:15:18 +0300

    Добавлено решение Задача: Основы Многопоточности (Thread и Runnable)

--- src/main/java/org/konneyj/module2advanced/Main.java
+++ src/main/java/org/konneyj/module2advanced/Main.java
@@ -5,12 +5,13 @@ import org.konneyj.module2advanced.notification.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         checkReliableClass();
         checkEqualAndHashCode();
         checkNotificationService();
         checkListAndHashMap();
         checkComparableAndComparator();
+        checkThreadAndRunnable();
     }
 
     public static void checkReliableClass() {
@@ -127,5 +128,39 @@ public class Main {
         }
 
         System.out.println("-".repeat(50));
+    }
+
+    public static void checkThreadAndRunnable() throws InterruptedException {
+        List<Downloader> loaders = new ArrayList<>(List.of(new Downloader(), new Downloader(), new Downloader(),
+                new Downloader(), new Downloader()));
+
+        long startTime1 = System.nanoTime();
+
+        for (Downloader loader : loaders) {
+            loader.run();
+        }
+        long endTime1 = System.nanoTime();
+        long diff1 = (endTime1 - startTime1) / 1_000_000_000;
+        System.out.println("Время выполнения потоков последовательно: " + diff1 + " секунд");
+
+        List<Thread> threads = new ArrayList<>();
+        Downloader downloader = new Downloader();
+        Thread thread1 = new Thread(downloader);
+        Thread thread2 = new Thread(downloader);
+        Thread thread3 = new Thread(downloader);
+        Thread thread4 = new Thread(downloader);
+        Thread thread5 = new Thread(downloader);
+
+        threads.addAll(List.of(thread1, thread2, thread3, thread4, thread5));
+        long startTime2 = System.nanoTime();
+
+        for (Thread thr : threads) {
+            thr.start();
+        }
+        thread5.join();
+
+        long endTime2 = System.nanoTime();
+        long diff2 = (endTime2 - startTime2) / 1_000_000_000;
+        System.out.println("Время выполнения потоков параллельно: " + diff2 + " секунд");
     }
 }
