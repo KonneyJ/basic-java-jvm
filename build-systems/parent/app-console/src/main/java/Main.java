727f459437ff4391427d1878028be82d1e77aa64
KonneyJ <konnova.j@yandex.ru>
2025-11-12 14:07:24 +0300

    Доработана Задача: Создание multi-module проекта

--- build-systems/parent/app-console/src/main/java/Main.java
+++ build-systems/parent/app-console/src/main/java/Main.java
@@ -2,7 +2,7 @@ package main.java;
 
 public class Main {
     public static void main(String[] args) {
-        CoreClass core = new CoreClass();
+        main.java.CoreClass core = new main.java.CoreClass();
         core.doBusinessLogic();
     }
 }
