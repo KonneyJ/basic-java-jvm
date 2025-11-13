a069b7e192d13442092604a59c994b56225d42d5
KonneyJ <konnova.j@yandex.ru>
2025-11-13 16:46:32 +0300

    Добавлено решение Задача: Qualifier для разрешения конфликтов

---
+++ spring-introduction/src/main/java/org/konneyj/service/notificationservice/EmailNotificationService.java
@@ -0,0 +1,7 @@
+package org.konneyj.service.notificationservice;
+
+import org.springframework.stereotype.Service;
+
+@Service("emailNotificationService")
+public class EmailNotificationService implements NotificationService {
+}
