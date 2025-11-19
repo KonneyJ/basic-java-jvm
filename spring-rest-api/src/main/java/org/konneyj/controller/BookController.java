2da62cce14f54318fd879b0c563c3c3669a2bc75
KonneyJ <konnova.j@yandex.ru>
2025-11-19 16:13:17 +0300

    Добавлено решение Задача: Параметры пути (@PathVariable)

--- spring-rest-api/src/main/java/org/konneyj/controller/BookController.java
+++ spring-rest-api/src/main/java/org/konneyj/controller/BookController.java
@@ -2,12 +2,18 @@ package org.konneyj.controller;
 
 import org.konneyj.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
     @GetMapping("/book")
     public Book getBook() {
-        return new Book("Вино из одуванчиков", "Рей Бредбери", 1957, 250);
+        return new Book(1, "Вино из одуванчиков", "Рей Бредбери", 1957, 250);
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBookById(@PathVariable int id) {
+        return new Book(id, "BookName", "BookAuthor", 1900, 100);
     }
 }
--- spring-rest-api/src/main/java/org/konneyj/model/Book.java
+++ spring-rest-api/src/main/java/org/konneyj/model/Book.java
@@ -10,6 +10,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
+    private int id;
     private String title;
     private String author;
     private int publishedYear;
