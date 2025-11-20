078385a0835c56038ff618ccb1e3682c2ab40826
KonneyJ <konnova.j@yandex.ru>
2025-11-20 16:59:40 +0300

    Добавлено решение Задача: Создание первой сущности (@Entity)

---
+++ spring-data-jpa/src/main/java/org/konneyj/model/Book.java
@@ -0,0 +1,34 @@
+package org.konneyj.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "published_year")
+    private Integer publishedYear;
+}
