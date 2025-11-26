f256195817f3121e248f3279e13ca207ba7e855c
KonneyJ <konnova.j@yandex.ru>
2025-11-26 18:00:26 +0300

    Добавлено решение Задача: Настройка связей между сущностями (@ManyToOne)

--- spring-data-jpa/src/main/java/org/konneyj/model/Author.java
+++ spring-data-jpa/src/main/java/org/konneyj/model/Author.java
@@ -5,32 +5,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "books")
-public class Book {
+@Table(name = "authors")
+public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "title")
-    private String title;
+    @Column(name = "name")
+    private String name;
 
-    @ManyToOne
-    @Column(name = "author")
-    private Author author;
+    @Column(name = "surname")
+    private String surname;
 
-    @Column(name = "published_year")
-    private Integer publishedYear;
+    @OneToMany(mappedBy = "author")
+    private final List<Book> books = new ArrayList<>();
 }
